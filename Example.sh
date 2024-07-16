05c559b34bf83b08eded99cecb43d552fe61f2ab
Xikai Shan <58974183+xkshan97@users.noreply.github.com>
2024-07-16 11:56:05 +0800

    Update Example.sh

--- Example.sh
+++ Example.sh
@@ -1,1 +1,1 @@
-g++ Example.cpp ./Micro_field_adaptive.cpp ./GetPsi_micro_field.cpp ./CosMoCal.cpp ./SampleMethod/RejectAndAcceptSample.cpp -lm -std=c++11 -O3 -g -pthread -o BigSampleRes
+g++ Example.cpp ./Micro_field_adaptive.cpp ./GetPsi_micro_field.cpp ./CosMoCal.cpp ./SampleMethod/RejectAndAcceptSample.cpp -lm -std=c++11 -O3 -g -pthread -o Example
